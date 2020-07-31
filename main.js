1ec0560673ca766cd7be719fba5f03f305a7e4e8
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-07-31 05:00:14 -0400

    done

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
-const makeDino = function() {
+// const dinosaur = {
+//   species: "trex",
+//   period: "modern",
+//   carnivore: true,
+//   extinct: true,
+// };
+
+const makeDino = function(name, era, eatsMeat, isExtinct = false) {
+  const dinosaur = {
+    species: name,
+    period: era,
+    carnivore: eatsMeat,
+    extinct: isExtinct,
+  };
+  return dinosaur;
+}
+// makeDino('trex', 'modern', true) //?
+
+const makeSingular = function(obj) {
+  let newDino = makeDino(obj.species, obj.period, obj.carnivore, obj.extinct);
+    if (obj.species.slice(-2) === "us") {
+      newDino.species = newDino.species.slice(0, -2);
+      return newDino;
+    } else {
+      return newDino;
+    }
+    }  
+
+const truncateSpecies = function(obj) {
+  let newDino = makeDino(obj.species, obj.period, obj.carnivore, obj.extinct);
+    if (obj.species.length > 10) {
+      newDino.species = newDino.species.slice(0, 7) + "...";
+      return newDino
+    } else {
+      return newDino;
+    }
 }
 
-const makeSingular = function() {
+
+
+
+
+const makeExtinct = function(obj) {
+  let newDino = makeDino(obj.species, obj.period, obj.carnivore, true)
+    return newDino;
 }
 
-const truncateSpecies = function() {
-}
+  
 
-const makeExtinct = function() {
 
-}
 
 module.exports = {
   makeDino,
